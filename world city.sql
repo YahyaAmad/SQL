fcee6be496382286398bec418e0f890f8101b02d
Yahya Amad <154363192+YahyaAmad@users.noreply.github.com>
2024-01-15 01:46:09 +0100

    Add files via upload

---
+++ world city.sql
@@ -0,0 +1,12 @@
+SELECT * FROM world.city;
+
+select name, id from world.city;
+
+select Countrycode from world.city;
+select distinct Countrycode from world.city;
+
+SELECT * FROM world.city
+where countrycode='afg';
+
+select * from world.city
+order by countrycode asc;
